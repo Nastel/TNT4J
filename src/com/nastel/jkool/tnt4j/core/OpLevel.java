b514ef8cb0de263582f09b24b8c1a722bca48980
Albert <amavashev@nastel.com>
2014-05-10 09:26:09 -0400

    Apache license, copyright update

--- src/com/nastel/jkool/tnt4j/core/OpLevel.java
+++ src/com/nastel/jkool/tnt4j/core/OpLevel.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2014 Nastel Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.nastel.jkool.tnt4j.core;
 
 
@@ -11,6 +26,21 @@ public enum OpLevel {
 	UNKNOWN, TRACE, DEBUG, INFO, SUCCESS, WARNING, ERROR, FAILURE, CRITICAL, FATAL, HALT;
 
 	private static OpLevel[] enumList = OpLevel.values();
+/*
+ * Copyright 2014 Nastel Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 	/**
 	 * Converts the specified value to a member of the enumeration.
