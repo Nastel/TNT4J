ba61fc688f3915c8e3ad00c66671c089ff12a2b6
Albert <amavashev@nastel.com>
2014-05-10 09:48:21 -0400

    fixed package typo

--- src/com/nastel/jkool/tnt4j/config/Configurable.java
+++ src/com/nastel/jkool/tnt4j/config/Configurable.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-ackage com.nastel.jkool.tnt4j.config;
+package com.nastel.jkool.tnt4j.config;
 
 import java.util.Map;
 
