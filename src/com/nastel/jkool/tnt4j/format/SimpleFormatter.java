67feaf76f48e340c66d89af4f8ea28b3757a51e0
AlbertM <amavashev@nastel.com>
2014-06-23 15:27:25 -0400

    Modified SimpleFormatter to improve log readability
    Modified SimpleFormatter to improve log readability

--- src/com/nastel/jkool/tnt4j/format/SimpleFormatter.java
+++ src/com/nastel/jkool/tnt4j/format/SimpleFormatter.java
@@ -26,7 +26,9 @@ import com.nastel.jkool.tnt4j.tracker.TrackingEvent;
  * <p>
  * Simple implementation of <code>Formatter</code> interface provides simple/minimal formatting of
  * <code>TrackingActvity</code> and <code>TrackingEvent</code> as well as any object passed to <code>format()</code>
- * method call.
+ * method call. Event entries are formatte as follows:
+ * <code>event-text-msg {event-tracking-info}</code>
+ * where <code>event-tracking-info</code> consists of <code>"name: value"</code> pairs.
  * </p>
  * 
  * 
@@ -43,36 +45,36 @@ public class SimpleFormatter extends DefaultFormatter {
 	@Override
 	public String format(TrackingEvent event) {
 		StringBuffer msg = new StringBuffer(1024);
-		msg.append("{'").append(event.getSeverity()).append(SEPARATOR);
-		msg.append("event='").append(event.getStringMessage()).append(SEPARATOR);
-		msg.append("name='").append(event.getOperation().getName()).append(SEPARATOR);
-		msg.append("ccode='").append(event.getOperation().getCompCode()).append(SEPARATOR);
+		msg.append(event.getStringMessage()).append(" ");
+		msg.append("{sev: '").append(event.getSeverity()).append(SEPARATOR);
+		msg.append("name: '").append(event.getOperation().getName()).append(SEPARATOR);
+		msg.append("ccode: '").append(event.getOperation().getCompCode()).append(SEPARATOR);
 		if (event.getOperation().getReasonCode() != 0) {
-			msg.append("rcode='").append(event.getOperation().getReasonCode()).append(SEPARATOR);
+			msg.append("rcode: '").append(event.getOperation().getReasonCode()).append(SEPARATOR);
 		}
 		if (event.getOperation().getElapsedTime() != 0) {
-			msg.append("usec='").append(event.getOperation().getElapsedTime()).append(SEPARATOR);
+			msg.append("usec: '").append(event.getOperation().getElapsedTime()).append(SEPARATOR);
 		}
 		if (event.getOperation().getMessageAge() != 0) {
-			msg.append("age.usec='").append(event.getOperation().getMessageAge()).append(SEPARATOR);
+			msg.append("age.usec: '").append(event.getOperation().getMessageAge()).append(SEPARATOR);
 		}
 		if (event.getOperation().getWaitTime() != 0) {
-			msg.append("wait.usec='").append(event.getOperation().getWaitTime()).append(SEPARATOR);
+			msg.append("wait.usec: '").append(event.getOperation().getWaitTime()).append(SEPARATOR);
 		}
 		if (event.getTag() != null) {
-			msg.append("tag='").append(event.getTag()).append(SEPARATOR);
+			msg.append("tag: '").append(event.getTag()).append(SEPARATOR);
 		}
 		if (event.getOperation().getCorrelator() != null) {
-			msg.append("corr-id='").append(event.getOperation().getCorrelator()).append(SEPARATOR);
+			msg.append("corr-id: '").append(event.getOperation().getCorrelator()).append(SEPARATOR);
 		}
 		if (event.getOperation().getThrowable() != null) {
-			msg.append("error='").append(event.getOperation().getExceptionString()).append(SEPARATOR);
+			msg.append("error: '").append(event.getOperation().getExceptionString()).append(SEPARATOR);
 		}
-		msg.append("type='").append(event.getOperation().getType()).append(SEPARATOR);
+		msg.append("type: '").append(event.getOperation().getType()).append(SEPARATOR);
 		if (event.getParentItem() != null) {
-			msg.append("parent-id='").append(event.getParentItem().getTrackingId()).append(SEPARATOR);
+			msg.append("parent-id: '").append(event.getParentItem().getTrackingId()).append(SEPARATOR);
 		}
-		msg.append("track-id='").append(event.getTrackingId()).append("'");
+		msg.append("track-id: '").append(event.getTrackingId()).append("'");
 		msg.append("}");
 		return msg.toString();
 	}
@@ -81,35 +83,35 @@ public class SimpleFormatter extends DefaultFormatter {
 	public String format(TrackingActivity activity) {
 		StringBuffer msg = new StringBuffer(1024);
 		msg.append("{'").append(activity.getStatus()).append(SEPARATOR);
-		msg.append("name='").append(activity.getName()).append(SEPARATOR);
+		msg.append("name: '").append(activity.getName()).append(SEPARATOR);
 		if (activity.getElapsedTime() != 0) {
-			msg.append("usec='").append(activity.getElapsedTime()).append(SEPARATOR);
+			msg.append("usec: '").append(activity.getElapsedTime()).append(SEPARATOR);
 		}
 		if (activity.getWaitTime() != 0) {
-			msg.append("wait.usec='").append(activity.getWaitTime()).append(SEPARATOR);
+			msg.append("wait.usec: '").append(activity.getWaitTime()).append(SEPARATOR);
 		}
 		if (activity.getStartTime() != null) {
-			msg.append("start.time='").append(activity.getStartTime()).append(SEPARATOR);
+			msg.append("start.time: '").append(activity.getStartTime()).append(SEPARATOR);
 		}
 		if (activity.getEndTime() != null) {
-			msg.append("end.time='").append(activity.getEndTime()).append(SEPARATOR);
+			msg.append("end.time: '").append(activity.getEndTime()).append(SEPARATOR);
 		}
 		if (activity.getThrowable() != null) {
-			msg.append("error='").append(activity.getExceptionString()).append(SEPARATOR);
+			msg.append("error: '").append(activity.getExceptionString()).append(SEPARATOR);
 		}
-		msg.append("pid='").append(activity.getPID()).append(SEPARATOR);
-		msg.append("tid='").append(activity.getTID()).append(SEPARATOR);
-		msg.append("source='").append(activity.getSource().getFQName()).append(SEPARATOR);
+		msg.append("pid: '").append(activity.getPID()).append(SEPARATOR);
+		msg.append("tid: '").append(activity.getTID()).append(SEPARATOR);
+		msg.append("source: '").append(activity.getSource().getFQName()).append(SEPARATOR);
 		if (activity.getParentItem() != null) {
-			msg.append("parent-id='").append(activity.getParentItem().getTrackingId()).append(SEPARATOR);
+			msg.append("parent-id: '").append(activity.getParentItem().getTrackingId()).append(SEPARATOR);
 		}
-		msg.append("track-id='").append(activity.getTrackingId()).append("'");
+		msg.append("track-id: '").append(activity.getTrackingId()).append("'");
 		if (activity.getSnapshotCount() > 0) {
 			List<PropertySnapshot> snapshots = activity.getSnapshots();
 			for (PropertySnapshot snap : snapshots) {
 				msg.append("\n\tSnapshot(").append(snap.getName()).append("@").append(snap.getCategory()).append(") {");
 				for (Property prop : snap) {
-					msg.append("\n\t\t").append(prop.getKey()).append("=").append(prop.getValue());
+					msg.append("\n\t\t").append(prop.getKey()).append(": ").append(prop.getValue());
 				}
 				msg.append("\n\t}");
 			}
