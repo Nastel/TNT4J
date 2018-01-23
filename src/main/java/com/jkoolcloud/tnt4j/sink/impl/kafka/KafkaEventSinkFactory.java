c3a0e7d23427eaebce248f248c986cc82085bab1
akausinis <andrius.kausinis@singleton-labs.lt>
2018-01-23 13:42:17 +0200

    Made ability to set Kafka producer properties directly in tnt4j.properties under Kafka sink stanza.

--- src/main/java/com/jkoolcloud/tnt4j/sink/impl/kafka/KafkaEventSinkFactory.java
+++ src/main/java/com/jkoolcloud/tnt4j/sink/impl/kafka/KafkaEventSinkFactory.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2015 JKOOL, LLC.
+ * Copyright 2014-2018 JKOOL, LLC.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -29,9 +29,8 @@ import com.jkoolcloud.tnt4j.sink.EventSinkFactory;
 import com.jkoolcloud.tnt4j.utils.Utils;
 
 /**
- * Concrete implementation of {@link EventSinkFactory} interface over Kafka, which
- * creates instances of {@link EventSink}. This factory uses {@link KafkaEventSink}
- * as the underlying provider.
+ * Concrete implementation of {@link EventSinkFactory} interface over Kafka, which creates instances of
+ * {@link EventSink}. This factory uses {@link KafkaEventSink} as the underlying provider.
  *
  *
  * @see EventSink
@@ -40,44 +39,48 @@ import com.jkoolcloud.tnt4j.utils.Utils;
  * @version $Revision: 1 $
  *
  */
-public class KafkaEventSinkFactory  extends AbstractEventSinkFactory {
+public class KafkaEventSinkFactory extends AbstractEventSinkFactory {
 	public static String DEFAULT_KAFKA_TOPIC = "tnt4j-topic";
 	public static String DEFAULT_KAFKA_PROP_FILE = "tnt4j-kafka.properties";
-	
+
 	private Properties kafkaProps = new Properties();
 	private String kafkaTopic = DEFAULT_KAFKA_TOPIC;
-	private String kafkaPropFile = DEFAULT_KAFKA_PROP_FILE;
-			
-	@Override
-    public EventSink getEventSink(String name) {
-	    return configureSink(new KafkaEventSink(kafkaTopic, kafkaProps, new JSONFormatter(false)));
-    }
+	private String kafkaPropFile = null;
 
 	@Override
-    public EventSink getEventSink(String name, Properties props) {
-	    return configureSink(new KafkaEventSink(kafkaTopic, kafkaProps, new JSONFormatter(false)));
-    }
+	public EventSink getEventSink(String name) {
+		return configureSink(new KafkaEventSink(kafkaTopic, kafkaProps, new JSONFormatter(false)));
+	}
 
 	@Override
-    public EventSink getEventSink(String name, Properties props, EventFormatter frmt) {
-	    return configureSink(new KafkaEventSink(kafkaTopic, kafkaProps, frmt));
-    }
+	public EventSink getEventSink(String name, Properties props) {
+		return configureSink(new KafkaEventSink(kafkaTopic, kafkaProps, new JSONFormatter(false)));
+	}
 
 	@Override
-    public void setConfiguration(Map<String, Object> settings) throws ConfigException {
+	public EventSink getEventSink(String name, Properties props, EventFormatter frmt) {
+		return configureSink(new KafkaEventSink(kafkaTopic, kafkaProps, frmt));
+	}
+
+	@Override
+	public void setConfiguration(Map<String, Object> settings) throws ConfigException {
 		super.setConfiguration(settings);
 		kafkaTopic = Utils.getString("topic", settings, DEFAULT_KAFKA_TOPIC);
-		kafkaPropFile = Utils.getString("propFile", settings, DEFAULT_KAFKA_PROP_FILE);
-		loadKafkaProps(kafkaPropFile, settings);
+		kafkaPropFile = Utils.getString("propFile", settings, kafkaPropFile);
+		if (kafkaPropFile != null) {
+			loadKafkaProps(kafkaPropFile, settings);
+		} else {
+			kafkaProps.putAll(settings);
+		}
 	}
-	
+
 	protected void loadKafkaProps(String fname, Map<String, Object> settings) throws ConfigException {
 		try {
-	        kafkaProps.load(new FileInputStream(new File(fname)));
-        } catch (Throwable e) {
-        	ConfigException error = new ConfigException(e.toString(), settings);
-        	error.initCause(e);
-        	throw error;
-        }		
+			kafkaProps.load(new FileInputStream(new File(fname)));
+		} catch (Throwable e) {
+			ConfigException error = new ConfigException(e.toString(), settings);
+			error.initCause(e);
+			throw error;
+		}
 	}
 }
