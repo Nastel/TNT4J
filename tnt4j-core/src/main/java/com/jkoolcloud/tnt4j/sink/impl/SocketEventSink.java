7f757dbb09455cc59fef0f4dfa637b4b976c4941
Andrius Kausinis <andrius.kausinis@singleton-labs.lt>
2023-03-09 08:51:12 +0200

    Added socket opening exception logging.

--- tnt4j-core/src/main/java/com/jkoolcloud/tnt4j/sink/impl/SocketEventSink.java
+++ tnt4j-core/src/main/java/com/jkoolcloud/tnt4j/sink/impl/SocketEventSink.java
@@ -25,6 +25,7 @@ import org.apache.commons.lang3.StringUtils;
 
 import com.jkoolcloud.tnt4j.core.OpLevel;
 import com.jkoolcloud.tnt4j.format.EventFormatter;
+import com.jkoolcloud.tnt4j.sink.DefaultEventSinkFactory;
 import com.jkoolcloud.tnt4j.sink.EventSink;
 import com.jkoolcloud.tnt4j.sink.LoggedEventSink;
 import com.jkoolcloud.tnt4j.tracker.TrackingActivity;
@@ -51,6 +52,8 @@ import com.jkoolcloud.tnt4j.utils.Utils;
  * @see EventFormatter
  */
 public class SocketEventSink extends LoggedEventSink {
+	private static final EventSink logger = DefaultEventSinkFactory.defaultEventSink(SocketEventSink.class);
+
 	private Socket socketSink = null;
 	private DataOutputStream outStream = null;
 	private String hostName = "localhost";
@@ -132,6 +135,8 @@ public class SocketEventSink extends LoggedEventSink {
 
 			super._open();
 		} catch (Throwable e) {
+			logger.log(OpLevel.ERROR, "Failed to open sink name={3}, host={0}, port={1}, proxy.address={2}", hostName,
+					String.valueOf(portNo), proxyAddr, getName(), e);
 			_close();
 
 			if (e instanceof IOException) {
